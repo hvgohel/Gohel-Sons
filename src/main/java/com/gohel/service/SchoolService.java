5e7258ddf4a38c2be7ef648f08d31f40dae212c0
hvgohel <hvgohel45@gmail.com>
2022-12-16 16:43:02 +0530

    Update SchoolService.java

--- src/main/java/com/gohel/service/SchoolService.java
+++ src/main/java/com/gohel/service/SchoolService.java
@@ -1,19 +1,37 @@
 package com.gohel.service;
 
+import com.gohel.model.Student;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import com.gohel.model.School;
 import com.gohel.repository.SchoolRepository;
 
+import java.util.List;
+
+
 @Service
+@RequiredArgsConstructor
 public class SchoolService extends AbstractService<School, Long> {
 
-  @Autowired
-  private SchoolRepository schoolRepository;
+  private final SchoolRepository schoolRepository;
+  private final UserService userService;
 
   @Override
   protected JpaRepository<School, Long> getRepository() {
     return schoolRepository;
+  }
+
+  public Page<School> getSchools(Integer pageNumber) {
+    return schoolRepository
+            .findAllByUser(userService.currentUser(), PageRequest.of(pageNumber - 1, 10, Sort.Direction.DESC, "id"));
+  }
+
+  public List<School> getAllSchool() {
+    return schoolRepository.findAllByUser(userService.currentUser());
   }
 }
