7253c4a823ef14b2d7737c49b86277cde061a59c
hvgohel <hvgohel45@gmail.com>
2022-12-16 16:39:45 +0530

    Update StudentRepository.java

--- src/main/java/com/gohel/repository/StudentRepository.java
+++ src/main/java/com/gohel/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.gohel.repository;
 
+import com.gohel.model.School;
+import com.gohel.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,11 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.gohel.model.Student;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-  Page<Student> findAllBySchoolIdNull(Pageable pageable);
+  Page<Student> findAllByUserAndSchoolIdNotNull(User user, Pageable pageable);
 
-  Page<Student> findAllBySchoolIdNotNull(Pageable pageable);
+  Page<Student> findAllByUserAndSchoolIdNull(User user, Pageable pageable);
+
+  Page<Student> findAllByUser(User user, Pageable pageable);
+
+  Page<Student> findAllByUserAndNameLike(User user, String name, Pageable pageable);
 
   @Query("select sum(s.price) from Student s where s.school.id = ?1")
   Integer sumBySchoolId(Long schoolId);
