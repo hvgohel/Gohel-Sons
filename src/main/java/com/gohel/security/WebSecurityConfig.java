64066f464f4d121ed244190ce44d45745b06bd94
hvgohel <hvgohel45@gmail.com>
2022-12-16 16:41:35 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/gohel/security/WebSecurityConfig.java
+++ src/main/java/com/gohel/security/WebSecurityConfig.java
@@ -1,34 +1,28 @@
 package com.gohel.security;
 
+import com.gohel.service.UserDetailServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource datasource;
+    private final UserDetailServiceImpl userDetailsServiceImpl;
 
     @Override
-  protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-      .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**").permitAll()
+      .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**", "/register").permitAll()
       .anyRequest().authenticated()
       .and()
     .formLogin()
@@ -41,29 +35,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
       .logoutSuccessUrl("/login")
       .permitAll();
-  }
-
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    // Use Spring Boots User detailsMAnager
-    JdbcUserDetailsManager userDetailsService = new JdbcUserDetailsManager();
-
-    // Set our Datasource to use the one defined in application.properties
-    userDetailsService.setDataSource(datasource);
-
-    // Create BCryptPassword encoder
-    PasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    // add components
-    auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-    auth.jdbcAuthentication().dataSource(datasource);
-
-    // add new user "user" with password "password" - password will be encrypted
-    if (!userDetailsService.userExists("dilip")) {
-      List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-      authorities.add(new SimpleGrantedAuthority("USER"));
-      User userDetails = new User("dilip", encoder.encode("dilip"), authorities);
-      userDetailsService.createUser(userDetails);
+   }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-  }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // add components
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+
+        /*List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        User userDetails = new User("dilip", encoder.encode("dilip"), authorities);*/
+    }
 }
