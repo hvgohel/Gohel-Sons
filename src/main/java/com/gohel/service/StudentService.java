49e9f321dec36d1645f5d54c9d65dc538c101b1e
hvgohel <hvgohel45@gmail.com>
2022-12-16 16:43:22 +0530

    Update StudentService.java

--- src/main/java/com/gohel/service/StudentService.java
+++ src/main/java/com/gohel/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.gohel.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,10 +11,11 @@ import com.gohel.model.Student;
 import com.gohel.repository.StudentRepository;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService extends AbstractService<Student, Long> {
 
-  @Autowired
-  private StudentRepository studentRepository;
+  private final StudentRepository studentRepository;
+  private final UserService userService;
 
   @Override
   protected JpaRepository<Student, Long> getRepository() {
@@ -22,12 +24,22 @@ public class StudentService extends AbstractService<Student, Long> {
 
   public Page<Student> getStudents(Integer pageNumber) {
     return studentRepository
-        .findAllBySchoolIdNotNull(PageRequest.of(pageNumber - 1, 10, Sort.Direction.DESC, "id"));
+        .findAllByUserAndSchoolIdNotNull(userService.currentUser(), PageRequest.of(pageNumber - 1, 10, Sort.Direction.DESC, "id"));
   }
 
   public Page<Student> getCustomers(Integer pageNumber) {
-    return studentRepository.findAllBySchoolIdNull(
+    return studentRepository.findAllByUserAndSchoolIdNull(userService.currentUser(),
         PageRequest.of(pageNumber - 1, 10, Sort.Direction.DESC, "deliveryDate"));
+  }
+
+  public Page<Student> getAllByUser(Integer pageNumber) {
+    return studentRepository.findAllByUser(userService.currentUser(),
+            PageRequest.of(pageNumber - 1, 20, Sort.Direction.DESC, "deliveryDate"));
+  }
+
+  public Page<Student> getAllByUserAndName(Integer pageNumber, String name) {
+    return studentRepository.findAllByUserAndNameLike(userService.currentUser(), "%" + name + "%",
+            PageRequest.of(pageNumber - 1, 20, Sort.Direction.ASC, "deliveryDate"));
   }
 
   public Integer getTotalAmount(Long schoolId) {
