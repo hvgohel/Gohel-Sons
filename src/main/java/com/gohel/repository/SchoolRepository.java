217e4197b67abf332f852f7f4f725f2bb27ba570
hvgohel <hvgohel45@gmail.com>
2022-12-16 16:39:28 +0530

    Update SchoolRepository.java

--- src/main/java/com/gohel/repository/SchoolRepository.java
+++ src/main/java/com/gohel/repository/SchoolRepository.java
@@ -1,9 +1,17 @@
 package com.gohel.repository;
 
+import com.gohel.model.Student;
+import com.gohel.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.gohel.model.School;
 
+import java.util.List;
+
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
+    Page<School> findAllByUser(User user, Pageable pageable);
+    List<School> findAllByUser(User user);
 }
